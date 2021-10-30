1abb189b407fdd98589b9ea2fe6c588b7fd79a6f
miihfranca <michelefdf@gmail.com>
2021-10-29 22:31:39 -0300

    atividades

---
+++ aula-react/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import  CClassecomponentes from './componentes/CClassecomponente';
+import Style from './componentes/Style.css'
+
+
+function App() {
+  return (
+    <div className="App">
+
+<div className="cor">
+                <h1>Olá Mundo React</h1>
+                <h1> Esse é meu componente de classe 🤪 </h1>
+            </div>
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ aula-react/src/componentes/CClassecomponente.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class  CClassecomponentes extends React.Component{
+
+  render(){
+        return(
+            <h1> Esse é meu componente de classe 🤪 </h1>
+        )
+  }
+
+}
+export default CClassecomponentes
